9c3f7c76d7e7f79df7f8b4d326dc7b70a02f6a8c
Rob Tucker <rtucker@mozilla.com>
2016-02-08 15:34:01 -0500

    Added config.py to git repo

---
+++ config.py
@@ -0,0 +1,42 @@
+import ConfigParser
+import os
+
+
+class ConfigObject(object):
+    host = None
+    username = None
+    password = None
+
+    def __init__(self, config_object, ini_section):
+        try:
+            self.host = config_object.get(ini_section, 'host')
+        except:
+            print 'host not in ini file'
+
+        try:
+            self.username = config_object.get(ini_section, 'username')
+        except:
+            print 'username not in ini file'
+
+        try:
+            self.password = config_object.get(ini_section, 'password')
+        except:
+            print 'password not in ini file'
+
+
+def read_config_object(filepath):
+    read_config = ConfigParser.RawConfigParser()
+    read_config.read(filepath)
+    return read_config
+
+
+def get_config(filepath=None, ini_section='InfoBlox'):
+    if not os.path.exists(filepath):
+        return None
+
+    if filepath is None:
+        return None
+
+    read_config = read_config_object(filepath)
+    ret_object = ConfigObject(read_config, ini_section)
+    return ret_object
