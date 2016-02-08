d924757c33f0df84d347f1952001798f81a47560
Rob Tucker <rtucker@mozilla.com>
2016-02-08 15:35:03 -0500

    Added network files to git repo

---
+++ network.py
@@ -0,0 +1,263 @@
+from common import make_request
+import json
+import yaml
+import sys
+from apioutput import APIOutput
+from basemixin import BaseMixin
+
+
+class Network(BaseMixin):
+
+    def get_network(self, network):
+        return self.search_by_ipv4addr(network)
+
+    def search_by_ipv4addr(self, ipv4addr, should_return=False, attrs=None):
+        if attrs is None:
+            url = 'network?network=%s' % ipv4addr
+        else:
+            url = 'network?network=%s&_return_fields=%s' % (ipv4addr, attrs)
+        ret = self.make_request(
+            url,
+            'get',
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        if should_return:
+            return ret
+        else:
+            print self.get_output(ret, self.o_format, self.delimeter)
+
+
+    def list_networks(self):
+        url = 'network'
+        ret = self.make_request(
+            url,
+            'get',
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        print self.get_output(ret, self.o_format, self.delimeter)
+
+    def create_option(self, network, option_name, option_value):
+        net_obj = self.search_by_ipv4addr(network, should_return=True)
+        try:
+            url = net_obj.json()[0]['_ref']
+        except (IndexError, KeyError):
+            print "Unable to find fixed address by ip"
+            sys.exit(2)
+        data = {}
+        options = []
+        options = [{
+                'name': option_name,
+                'value': option_value
+            }]
+        data['options'] = options
+        ret_obj = self.make_request(
+            url,
+            'update',
+            data=data,
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        try:
+            if ret_obj.status_code == 200:
+                print "Successfully created network option"
+            else:
+                print "Unable to create network option"
+                print ret_obj.json()['text']
+        except Exception, e:
+            print "Unable to create network option"
+            sys.exit(2)
+
+    def create_attr(self, network, attr_name, attr_value):
+        net_obj = self.search_by_ipv4addr(
+            network,
+            should_return=True,
+            attrs="extattrs"
+        )
+        import pdb; pdb.set_trace()
+        try:
+            url = net_obj.json()[0]['_ref']
+        except (IndexError, KeyError):
+            print "Unable to find fixed address by ip"
+            sys.exit(2)
+        extattrs = {}
+        try:
+            extattrs['extattrs'] = net_obj.json()[0]['extattrs']
+        except:
+            extattrs['extattrs'] = {}
+
+        extattrs['extattrs'][attr_name] = {"value": attr_value}
+        ret_obj = self.make_request(
+            url,
+            'update',
+            data=extattrs,
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        try:
+            if ret_obj.status_code == 200:
+                print "Successfully created network extattr"
+            else:
+                print "Unable to create network extattr"
+                print ret_obj.json()['text']
+        except Exception, e:
+            print "Unable to create network extattr"
+            sys.exit(2)
+    def add_option(self, network, option_name, option_value):
+        url = 'network?comment~:=%s' % name
+        ret = self.make_request(
+            url,
+            'get',
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        print self.get_output(ret, self.o_format, self.delimeter)
+
+    def search_by_name(self, name):
+        url = 'network?comment~:=%s' % name
+        ret = self.make_request(
+            url,
+            'get',
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        print self.get_output(ret, self.o_format, self.delimeter)
+
+    def search_by_site(self, site):
+        url = 'network?*Site~:=%s' % site
+        ret = self.make_request(
+            url,
+            'get',
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        print self.get_output(ret, self.o_format, self.delimeter)
+
+    def search(self, name=None, site=None):
+        if name is not None:
+            self.search_by_name(name=name)
+        elif site is not None:
+            self.search_by_site(site=site)
+
+    def search_by_network_cidr(self, network):
+        url = 'network?network~=%s' % network
+        network_obj = self.make_request(
+            url,
+            'get',
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        try:
+            ref = network_obj.json()[0]['_ref']
+        except (IndexError, KeyError):
+            return None
+        return self.make_request(
+            ref,
+            'get',
+            hostname=self.hostname,
+            auth=self.auth
+        )
+
+    def get(self, network):
+        ret = self.search_by_network_cidr(
+            network=network,
+        )
+        if ret is None:
+            print "Unable to get Nework: {network}".format(
+                network=network
+            )
+        else:
+            print self.get_output(ret, self.o_format, self.delimeter)
+
+    def fixedaddresses(self, network):
+        url = 'fixedaddress?network~=%s' % network
+        ret = self.make_request(
+            url,
+            'get',
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        try:
+            found_records_len = len(ret.json())
+        except:
+            found_records_len = 0
+        if len(ret.json()) == 0:
+            print 'No Networks Found'
+        print self.get_output(ret, self.o_format, self.delimeter)
+
+    def delete_network(self, network):
+        try:
+            network_req = self.search_by_network_cidr(
+                network=network,
+            )
+            network_ref = network_req.json()['_ref']
+        except (AttributeError, TypeError, KeyError):
+            return False
+        resp = self.make_request(
+            network_ref,
+            'delete',
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        try:
+            status_code = resp.status_code
+        except:
+            print "Unable to delete network: %s" % network
+            return False
+
+        if status_code == 200:
+            print "Network Successfully Deleted: %s" % network
+            return True
+        else:
+            print "Unable to delete network: %s" % network
+            return False
+    def create_range(
+            self,
+            name,
+            start,
+            end,
+            disable=True,
+            should_print=True):
+        url = "range"
+        data = {}
+        data['name'] = name
+        data['start_addr'] = start
+        data['end_addr'] = end
+        data['disable'] = disable
+        ret = make_request(
+            url,
+            'create',
+            data=data,
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        output = self.get_output(ret, self.o_format, self.delimeter)
+        # TODO: Print the object after a get range
+        # Really should refactor this into it's own module
+        if should_print is True:
+            print output
+
+    def create_network(
+            self,
+            network,
+            comment="",
+            disable=False,
+            members=[],
+            should_print=False):
+        url = "network"
+        data = {}
+        data['network'] = network
+        data['comment'] = comment
+        data['disable'] = disable
+        data['members'] = members
+        ret = make_request(
+            url,
+            'create',
+            data=data,
+            hostname=self.hostname,
+            auth=self.auth
+        )
+        output = self.get_output(ret, self.o_format, self.delimeter)
+        if should_print is True:
+            print output
---
+++ network_process_cli.py
@@ -0,0 +1,106 @@
+from network import Network
+
+
+def network_process_cli(config, auth, opt):
+    if opt['--delimeter'] is None:
+        delimeter = " "
+    else:
+        delimeter = opt['--delimeter']
+
+    if opt['--format'] is None:
+        o_format = "text"
+    else:
+        o_format = opt['--format']
+
+    n = Network(
+        hostname=config.host,
+        auth=auth,
+        o_format=o_format,
+        delimeter=delimeter
+    )
+
+    if opt['list'] is True:
+        n.list_networks()
+    elif opt['search'] is True:
+        n.search(
+            name=opt['<name>'],
+            site=opt['<site>'],
+        )
+    elif opt['get'] is True:
+        network = opt['<network>']
+        n.get(network)
+    elif opt['fixedaddresses'] is True:
+        network = opt['<network>']
+        n.fixedaddresses(network)
+    elif opt['option'] is True and opt['create']:
+        network_block = opt['<network_block>']
+        option_name = opt['<option>']
+        option_value = opt['<value>']
+        n.create_option(network_block, option_name, option_value)
+    elif opt['attr'] is True and opt['create']:
+        network_block = opt['<network_block>']
+        attr_name = opt['<option>']
+        attr_value = opt['<value>']
+        n.create_attr(network_block, attr_name, attr_value)
+    elif opt['create'] is True:
+        if not opt['range']:
+            network = opt['<network>']
+            name = opt['<name>']
+            disable = opt['--disable']
+
+            if disable is None:
+                disable = True
+
+            if isinstance(disable, str):
+                if disable.upper() == 'TRUE':
+                    disable = True
+                elif disable.upper() == 'FALSE':
+                    disable = False
+            try:
+                members = opt['--members'].split(',')
+            except AttributeError:
+                members = []
+            n.create_network(
+                network,
+                name,
+                disable=disable,
+                members=members,
+            )
+            if n.api_out.has_error:
+                print "Unable to Create Network: %s" % network
+                sys.exit(2)
+            else:
+                print "Network Created Successfully"
+                n.get(network)
+        elif opt['range'] is True:
+            start = opt['<start>']
+            end = opt['<end>']
+            name = opt['<name>']
+            comment = opt['--comment']
+            disable = opt['--disable']
+
+            if disable is None:
+                disable = True
+
+            # Make a method out of this
+            # def parse_disable
+            if isinstance(disable, str):
+                if disable.upper() == 'TRUE':
+                    disable = True
+                elif disable.upper() == 'FALSE':
+                    disable = False
+            n.create_range(
+                name,
+                start,
+                end,
+                disable=disable,
+            )
+
+    if opt['delete'] is True:
+        network = opt['<network>']
+        if network is not None:
+            ret = n.delete_network(
+                network,
+            )
+            if ret is False:
+                print "Unable to delete network"
