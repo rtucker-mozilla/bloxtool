9ca84e805fc5deba11f26282e56e58a0b2ab80ff
Rob Tucker <rtucker@mozilla.com>
2017-03-14 16:01:31 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bloxtool',
+      version='0.11',
+      description='Tool for interfacing with InfoBlox',
+      url='https://github.com/rtucker-mozilla/bloxtool',
+      author='Rob Tucker',
+      author_email='rtucker@mozilla.com',
+      license='Mozilla Public License',
+      packages=['bloxtool'],
+      zip_safe=False)
