f2896a98ed7b3696039383618f1f1a5d5becc087
Rob Tucker <rtucker@mozilla.com>
2017-05-12 10:33:18 -0400

    Incremented setup.py for version v0.29

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bloxtool',
-      version='0.28',
+      version='0.29',
       description='Tool for interfacing with InfoBlox',
       url='https://github.com/rtucker-mozilla/bloxtool',
       author='Rob Tucker',
